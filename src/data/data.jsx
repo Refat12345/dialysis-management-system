d8aaf22108dcc7d6c2c3fc3db55f35d7102d0a33
WaseemAlBizreh <waseemalbizreh.tec@gmail.com>
2024-05-01 09:30:59 +0300

    add user route

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -13,15 +13,16 @@ import OrdersIcon from "../assets/icons/medical-center/dashboard/SideBar/order.s
 import NotesIcon from "../assets/icons/medical-center/dashboard/SideBar/notification.svg";
 import SettingsIcon from "../assets/icons/medical-center/dashboard/SideBar/setting.svg";
 import LogOutIcon from "../assets/icons/medical-center/dashboard/SideBar/logOut.svg";
-import online from '../assets/icons/medical-center/users/users-list/online.svg'
-import doctor from '../assets/icons/medical-center/users/users-list/doctor.svg'
+import online from "../assets/icons/medical-center/users/users-list/online.svg";
+import doctor from "../assets/icons/medical-center/users/users-list/doctor.svg";
 
 //-------------------------
 
-//Routes 
+//Routes
 
 export const mainRoute = "/";
 export const patientsRoute = "/patients";
+export const usersRoute = "/users";
 
 //--------------------------
 export const titleSession = {
@@ -45,7 +46,7 @@ export const adminSideBar = {
   },
   items: [
     { href: mainRoute, name: "الصفحة الرئيسية", icon: HomePageICon },
-    { href: "/", name: "المستخدمين", icon: UsersIcon },
+    { href: usersRoute, name: "المستخدمين", icon: UsersIcon },
     { href: patientsRoute, name: "المرضى", icon: PatientIcon },
     { href: "", name: "المواعيد", icon: AppointmentsIcon },
     { href: "", name: "جلسات الغسيل", icon: DialysisSessionsIcon },
@@ -75,7 +76,6 @@ export const sessions = data.map((data, index) => {
 });
 
 export const cardsData = [
- 
   {
     name: "أسماء ديراني",
     role: "ممرضة",
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,12 +2,19 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import ErrorPage from "../pages/error/ErrorPage";
 import PatientListPage from "../pages/manager_center/patient/patient_list/PatientListPage";
-import { mainRoute, patientsRoute } from "../data/data";
+import { mainRoute, patientsRoute, usersRoute } from "../data/data";
+import UsersListPage from "../pages/manager_center/users/users-list/UsersListPage";
 
 const router = createBrowserRouter([
   {
     path: mainRoute,
     element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: usersRoute,
+    element: <UsersListPage />,
+    //TODO: u may have to add loader
     errorElement: <ErrorPage />,
   },
   {
