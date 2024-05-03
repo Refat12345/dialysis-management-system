193057c8fe0c29bb8d67f7a91b11be8486fa9c97
WaseemAlBizreh <waseemalbizreh.tec@gmail.com>
2024-05-03 09:25:18 +0300

    navbar v3

--- src/pages/manager_center/patient/patient_profile/PatientProfileState.jsx
+++ src/pages/manager_center/patient/patient_profile/PatientProfileState.jsx
@@ -28,11 +28,38 @@ export const PatientProfileStateProvider = ({ children }) => {
     { href: globalNotesRoute, name: globalNotes },
   ];
 
+  const tabScreens = {
+    //TODO: put patient screens here
+    globalInfo: <div>1</div>,
+    medicalRecord: <div>2</div>,
+    dialysisSessions: <div>3</div>,
+    medicalAnalysis: <div>4</div>,
+    prescriptions: <div>5</div>,
+    globalNotes: <div>6</div>,
+  };
+
   const [state, setState] = useState({
     activeItem: patientProfileMenuItems[0].name,
     patientProfileMenuItems: patientProfileMenuItems,
+    selectScreen: (val) => selectScreen(val),
   });
 
+  const selectScreen = (screen) => {
+    switch (screen) {
+      case globalInfo:
+        return tabScreens.globalInfo;
+      case dialysisSessions:
+        return tabScreens.dialysisSessions;
+      case medicalRecord:
+        return tabScreens.medicalRecord;
+      case medicalAnalysis:
+        return tabScreens.medicalAnalysis;
+      case prescriptions:
+        return tabScreens.prescriptions;
+      case globalNotes:
+        return tabScreens.globalNotes;
+    }
+  };
   const updateState = (newValues) => {
     setState((prevState) => ({
       ...prevState,
